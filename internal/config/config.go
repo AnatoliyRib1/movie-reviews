5dc63fbaa02b064f352318026a59b979aaa6a0b6
AnatoliyRib1 <anatoliy.rib@yandex.ru>
2023-05-18 10:30:11 +0300

    AnatoliyRib1/f1-graceful-shutdown

---
+++ internal/config/config.go
@@ -0,0 +1,19 @@
+package config
+
+import (
+	"fmt"
+	"github.com/caarlos0/env/v8"
+)
+
+type Config struct {
+	DbUrl string `env:"DB_URL"`
+	Port  int    `env:"PORT" envDefault: "8080"`
+}
+
+func NewConfig() (*Config, error) {
+	var c Config
+	if err := env.Parse(&c); err != nil {
+		return nil, fmt.Errorf("parse config: %w", err)
+	}
+	return &c, nil
+}

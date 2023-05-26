0688b61a0112085cf5acc43b7db89d3ec9be3725
AnatoliyRib1 <anatoliy.rib@yandex.ru>
2023-05-26 12:47:43 +0300

    AnatoliyRib/f4-errors

--- internal/modules/auth/middleware.go
+++ internal/modules/auth/middleware.go
@@ -1,42 +1,54 @@
 package auth
 
 import (
+	"github.com/AnatoliyRib1/movie-reviews/internal/apperrors"
 	"github.com/AnatoliyRib1/movie-reviews/internal/jwt"
 	"github.com/AnatoliyRib1/movie-reviews/internal/modules/users"
 	"github.com/labstack/echo/v4"
 )
+
+var errForbidden = apperrors.Forbidden("insufficient permissions")
 
 func Self(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(c echo.Context) error {
 		userId := c.Param("userId")
 
 		claims := jwt.GetClaims(c)
+		if claims == nil {
+			return errForbidden
+		}
 		if claims.Role == users.AdminRole || claims.Subject == userId {
 			return next(c)
 		}
 
-		return echo.ErrForbidden
+		return errForbidden
 	}
 }
 
 func Editor(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(c echo.Context) error {
 		claims := jwt.GetClaims(c)
+		if claims == nil {
+			return errForbidden
+		}
 		if claims.Role == users.AdminRole || claims.Role == users.EditorRole {
 			return next(c)
 		}
 
-		return echo.ErrForbidden
+		return errForbidden
 	}
 }
 
 func Admin(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(c echo.Context) error {
 		claims := jwt.GetClaims(c)
+		if claims == nil {
+			return errForbidden
+		}
 		if claims.Role == users.AdminRole {
 			return next(c)
 		}
 
-		return echo.ErrForbidden
+		return errForbidden
 	}
 }

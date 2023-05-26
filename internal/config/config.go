53d2a032249fa579c7d61f77de7ab8c0bb57bec6
AnatoliyRib1 <anatoliy.rib@yandex.ru>
2023-05-27 00:02:48 +0300

    AnatoliyRib1/f5-logging

--- internal/config/config.go
+++ internal/config/config.go
@@ -7,22 +7,24 @@ import (
 	"github.com/caarlos0/env/v8"
 )
 
-type AdminConfig struct {
-	Username string `env:"NAME" validate:"min=5,max=16"`
-	Email    string `env:"EMAIL" validate:"email"`
-	Password string `env:"PASSWORD" validate:"password"`
-}
-
 type Config struct {
-	DbUrl string      `env:"DB_URL"`
-	Port  int         `env:"PORT" envDefault:"8081"`
-	Jwt   JwtConfig   `envPrefix:"JWT_"`
-	Admin AdminConfig `envPrefix:"ADMIN_"`
+	DbUrl    string      `env:"DB_URL"`
+	Port     int         `env:"PORT" envDefault:"8081"`
+	Jwt      JwtConfig   `envPrefix:"JWT_"`
+	Admin    AdminConfig `envPrefix:"ADMIN_"`
+	Local    bool        `env:"LOCAL" envDefault:"false"`
+	LogLevel string      `env:"LOG_LEVEL" envDefault:"info"`
 }
 
 type JwtConfig struct {
 	Secret           string        `env:"SECRET"`
 	AccessExpiration time.Duration `env:"ACCESS_EXPIRATION" envDefault:"30m"`
+}
+
+type AdminConfig struct {
+	Username string `env:"NAME" validate:"min=5,max=16"`
+	Email    string `env:"EMAIL" validate:"email"`
+	Password string `env:"PASSWORD" validate:"password"`
 }
 
 func NewConfig() (*Config, error) {

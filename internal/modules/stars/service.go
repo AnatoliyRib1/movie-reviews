dd1d3205fa79bd7741448368e594861f1fa7d089
AnatoliyRib1 <anatoliy.rib@yandex.ru>
2023-06-04 20:32:21 +0300

    AnatoliyRib1/f9-pagination

--- internal/modules/stars/service.go
+++ internal/modules/stars/service.go
@@ -14,11 +14,10 @@ func NewService(repo *Repository) *Service {
 	return &Service{repo: repo}
 }
 
-/*
-	func (s *Service) GetAll(ctx context.Context) ([]*Genre, error) {
-		return s.repo.GetAll(ctx)
-	}
-*/
+func (s *Service) GetAllPaginated(ctx context.Context, offset int, limit int) ([]*Star, int, error) {
+	return s.repo.GetAllPaginated(ctx, offset, limit)
+}
+
 func (s *Service) GetByID(ctx context.Context, starID int) (star *Star, err error) {
 	return s.repo.GetByID(ctx, starID)
 }
@@ -31,20 +30,18 @@ func (s *Service) Create(ctx context.Context, star *Star) error {
 	return nil
 }
 
-/*func (s *Service) Update(ctx context.Context, genreID int, name string) error {
-	if err := s.repo.Update(ctx, genreID, name); err != nil {
+func (s *Service) Update(ctx context.Context, star *Star) error {
+	if err := s.repo.Update(ctx, star); err != nil {
 		return err
 	}
-	log.FromContext(ctx).Info("genre updated", "genreId", genreID, "genreName", name)
+	log.FromContext(ctx).Info("star updated", "starFirstName", star.FirstName, "starLastName", star.LastName)
 	return nil
 }
 
-func (s *Service) Delete(ctx context.Context, genreID int) error {
-	if err := s.repo.Delete(ctx, genreID); err != nil {
+func (s *Service) Delete(ctx context.Context, starID int) error {
+	if err := s.repo.Delete(ctx, starID); err != nil {
 		return err
 	}
-	log.FromContext(ctx).Info("genre deleted", "genreId", genreID)
+	log.FromContext(ctx).Info("star deleted", "starId", starID)
 	return nil
 }
-
-*/

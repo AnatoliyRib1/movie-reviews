59ec7859cac605305ccabb0385401d3d3ca6feb9
AnatoliyRib1 <anatoliy.rib@yandex.ru>
2023-06-29 15:51:44 +0300

    AnatoliyRib1/f16-scrapper

--- contracts/stars.go
+++ contracts/stars.go
@@ -40,9 +40,9 @@ func (r *GetStarsRequest) ToQueryParams() map[string]string {
 }
 
 type CreateStarRequest struct {
-	FirstName  string     `json:"first_name" validate:"min=1,max=50"`
+	FirstName  string     `json:"first_name" validate:"min=0,max=100"`
 	MiddleName *string    `json:"middle_name,omitempty" validate:"max=50"`
-	LastName   string     `json:"last_name" validate:"min=1,max=50"`
+	LastName   string     `json:"last_name"`
 	BirthDate  time.Time  `json:"birth_date" validate:"nonzero"`
 	BirthPlace *string    `json:"birth_place,omitempty" validate:"max=100"`
 	DeathDate  *time.Time `json:"death_date,omitempty"`
